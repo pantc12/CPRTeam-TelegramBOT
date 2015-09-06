a1022e0f3287726bace072b5d49f23b83de29867
PTC <pantc12@gmail.com>
2015-09-07 03:43:17 +0800

    Add Example config.php

---
+++ config.example.php
@@ -0,0 +1,13 @@
+<?php
+
+define('TOKEN', '');
+define('BOT_NAME', '');
+
+
+$cmd_list = array(
+    'Command' => 'Command Description',
+);
+
+$users = array(
+    00000000, // Telegram User ID
+);
