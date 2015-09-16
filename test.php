b1632d2396c4c6970f17537abe5c55fff2bb95c8
PTC <pantc12@gmail.com>
2015-09-17 00:32:51 +0800

    Add test2 funciton

--- test.php
+++ test.php
@@ -4,7 +4,7 @@ function test(){
     $fid = $GLOBALS['fromID'];
     $test = $GLOBALS['test'];
     if(in_array($fid, $test)){
-        $p = rand(1, 100) % 10;
+        $p = rand(1, 100) % 5;
         $m = rand(1, 100) % 3;
         $msg = '';
 
@@ -19,6 +19,34 @@ function test(){
 
             case 2:
                 $msg = '茲~茲~~茲~茲~~茲~茲~~';
+                break;
+
+            default:
+                break;
+        }
+
+        switch ($p) {
+            case 0:
+                sendMsg($msg, false);
+                break;
+            
+            default:
+                break;
+        }
+    }
+}
+
+function test2(){
+    $fid = $GLOBALS['fromID'];
+    $test2 = $GLOBALS['test2'];
+    if(in_array($fid, $test2)){
+        $p = rand(1, 100) % 3;
+        $m = rand(1, 100) % 1;
+        $msg = '';
+
+        switch ($m) {
+            case 0:
+                $msg = '阿不就好笑笑~ㄌㄩㄝ';
                 break;
 
             default:
